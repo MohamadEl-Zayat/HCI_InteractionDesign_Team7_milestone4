8b990e744d73d3e131853b179b35d091f284ce36
MohamedTElZayat <zmohamadtarek@gmail.com>
2018-12-17 17:58:09 +0200

    first commit

---
+++ Assets/Scripts/PrintScripts.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintScripts : MonoBehaviour
+{
+   public void printMarkerOne(){
+   	Application.OpenURL("https://drive.google.com/open?id=1aSnb9JTri9eNZRLl39tpY8gkAP2J5slH");
+   }
+
+   public void printMarkerTwo(){
+   	Application.OpenURL("https://drive.google.com/open?id=1lsErWIDu6XiLrI3QZR-XukVsqwz5wFge");
+   }
+
+   public void printMarkerThree(){
+   	Application.OpenURL("https://drive.google.com/open?id=1OASesvaXZ76p9m9j3oCgniGZSFJg9gmI");
+   }
+}
